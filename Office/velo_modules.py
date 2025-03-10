9e0432f310725f84ab8c703b4387bcc4213aa2d4
Steve McG Kerv <mcshine05@gmail.com>
2025-03-10 12:49:05 +0000

    velo mods + git ignore update

---
+++ Office/velo_modules.py
@@ -0,0 +1,105 @@
+import sys
+import json
+from pathlib import Path
+import requests
+import datetime
+
+sys.path.append ('/Users/stevemcgarry/Projects/VS_Python/Tools')
+from gyro_tools import *
+
+def listEdges(vco,enterprise,api_key,output_list,timestamp):
+    vco_hostname = vco
+    enterprise_id = enterprise
+    token = api_key
+    base_output = output_list
+    suffix = timestamp
+
+    edges_output = f'{base_output}/vce_list_full-{suffix}.json'
+
+    # >>> API call
+    vco_url = f'https://{vco_hostname}/portal/rest/'
+    headers = {"Content-Type": "application/json", "Authorization": token}
+    get_edgeconfig = f'{vco_url}enterprise/getEnterpriseEdges'
+
+    getConfig_params = {'enterpriseId': enterprise_id}
+
+    config_reponse = requests.post(get_edgeconfig, headers= headers, data=json.dumps(getConfig_params))
+    c_resp = config_reponse.json()
+
+    with open(edges_output,'w') as file:
+        json.dump(c_resp, file)
+
+    vce_dict = {}
+    site_count = 0
+    for i in c_resp:
+        site_count += 1
+        vce_dict[f'{i["name"]}'] = i["id"]
+
+    return site_count, vce_dict
+
+def getEdgeConfig(vco,enterprise,api_key,output_dir,timestamp,edge_name,edge_id):
+    vco_hostname = vco
+    enterprise_id = enterprise
+    token = api_key
+    base_output = output_dir
+    suffix = timestamp
+    edge_name = edge_name
+    edge_id = edge_id
+
+    config_output = f'{base_output}/vce_configs/{edge_name}-{suffix}.json'
+
+    # API call
+    vco_url = f'https://{vco_hostname}/portal/rest/'
+    headers = {"Content-Type": "application/json", "Authorization": token}
+    get_edgeconfig = f'{vco_url}edge/getEdgeConfigurationStack'
+
+    getConfig_params = {'edgeId': edge_id,
+            'enterpriseId': enterprise_id}
+
+    config_reponse = requests.post(get_edgeconfig, headers= headers, data=json.dumps(getConfig_params))
+    c_resp = config_reponse.json()
+    edgeSpecificProfile = dict(c_resp[0])
+
+    with open(config_output,'w') as file:
+        json.dump(edgeSpecificProfile, file)
+    
+    return edgeSpecificProfile
+
+def wanLinkSummary(edgeSpecificProfile):
+    edgeSpecificProfile = edgeSpecificProfile
+    modules = edgeSpecificProfile['modules']
+    
+    count = 0
+    for m in modules:
+        # print(m['name'])
+        # print(count)
+        if m['name'] == 'WAN':
+            # wan_links = f"{edgeSpecificProfile}['modules'][{count}]['data']['links']"
+            wan_links = edgeSpecificProfile['modules'][count]['data']['links']
+        else:
+            count += 1
+    # wan_links = edgeSpecificProfile['modules'][5]['data']['links']
+    wan_count = (len(wan_links))
+
+    network_list = []
+    for i in range(wan_count):
+            sublist = []
+            sublist.append(wan_links[i]['name'])
+            sublist.append(wan_links[i]['publicIpAddress'])
+            sublist.append(wan_links[i]['type'])
+            sublist.append(wan_links[i]['isp'])
+            sublist.append(wan_links[i]['interfaces'][0])
+            # > last avtive time
+            # last = (edgeSpecificProfile['modules'][5]['data']['links'][i]['lastActive'])
+            # date_time = datetime.datetime.fromtimestamp(last/1000)
+            # formatted_date = date_time.strftime('%Y-%m-%d')
+            # sublist.append(formatted_date)
+            backup = (wan_links[i]['backupOnly'])
+            standby = (wan_links[i]['hotStandby'])
+            if backup or standby:
+                    sublist.append('Yes')
+            else:
+                    sublist.append('No')
+            network_list.append(sublist)
+    
+    return wan_count, network_list
---
+++ Office/velo_wan_reporter.py
@@ -0,0 +1,62 @@
+import sys
+import datetime
+import csv
+
+sys.path.append ('/Users/stevemcgarry/Projects/VS_Python/Tools')
+from gyro_tools import *
+from velo_modules import *
+
+# >>> Required 
+vco = 'vco211-fra1.velocloud.net'
+enterprise = 945
+api_key = f'Token {api_template("velo_211")}'
+output_dir = '/Users/stevemcgarry/Projects/Velo-Websockets/Office/output'
+# output_dir = '/Users/stevemcgarry/Projects/Velo-Websockets/Office/output/vce_configs'
+now = datetime.datetime.now()
+timestamp = now.strftime('%d%m%y')
+vce_dict_list = f'{output_dir}/vce_dict.txt'
+csv_file = f'{output_dir}/device_wan-links-{timestamp}.csv'
+
+# >>> start of code
+clrscr()
+print('\nREPORT GENERATION STARTED\n')
+count,vce_dict = listEdges(vco,enterprise,api_key,output_dir,timestamp)
+with open(vce_dict_list,'w') as dict_file:
+    for key, value in vce_dict.items():
+        dict_file.write(f'{key}: {value}\n')
+
+print('List of edges discovered...')
+print(f'Sites found: {count}\n')
+
+csv_headers = ['site_name', 'wan_total', 'wan0_label', 'wan0_ip', 'wan0_type', 'wan0_isp', 'wan0_interface', 'backup/standby',
+                'wan1_label', 'wan1_ip', 'wan1_type', 'wan1_isp', 'wan1_interface', 'backup/standby',
+                'wan2_label', 'wan2_ip', 'wan2_type', 'wan2_isp', 'wan2_interface', 'backup/standby']
+
+#  open csv file add header then loop through devices
+with open(csv_file, mode='w', newline='') as file:
+        writer = csv.writer(file)
+        writer.writerow(csv_headers)
+        for key,value in vce_dict.items():
+            edge_name = key
+            edge_id = value
+            print(f'{edge_name} - {edge_id}')
+            edgeSpecificProfile = getEdgeConfig(vco,enterprise,api_key,output_dir,timestamp,edge_name,edge_id)
+            wan_count, network_list = wanLinkSummary(edgeSpecificProfile)
+            
+            #  generate csv content
+            csv_line = edge_name
+            links = 1
+            while links <= wan_count:
+                csv_line = f'{csv_line},{wan_count}'
+                csv_line = f'{csv_line},{network_list[links - 1][0]}'
+                csv_line = f'{csv_line},{network_list[links - 1][1]}'
+                csv_line = f'{csv_line},{network_list[links - 1][2]}'
+                csv_line = f'{csv_line},{network_list[links - 1][3]}'
+                csv_line = f'{csv_line},{network_list[links - 1][4]}'
+                csv_line = f'{csv_line},{network_list[links - 1][5]}'
+                links += 1
+
+            row_data = csv_line.split(',')
+            writer.writerow(row_data)
+
+print('\nREPORT COMPLETED 8-)')
